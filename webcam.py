d8156e6ad36b258008370cc44a36f034e0370994
Garvishb <togarvish@gmail.com>
2024-03-15 18:17:00 -0400

    WORKING!!!!

--- webcam.py
+++ webcam.py
@@ -1,5 +1,15 @@
 import numpy as np
 import cv2 as cv
+import time
+
+
+
+# New idea:
+# Get the bounding box of the surface       
+# Get the coordinates of the origin of the bounding box (left bottom corner) in camera frame
+# Get a transformation matrix from camera frame to the surface frame
+# Apply the transformation matrix to the coordinates of the recyclable garbage
+# Calculate where it lies in the in the grid (might need a pixel to inch conversion?)
 
 
 class GirdDetect():
@@ -28,13 +38,13 @@ class GirdDetect():
             img_roi = img.copy()
             # getting contours
             surface_coordinates = self.get_contours(canny, img_copy)
-            print("Surface coordinates: ", surface_coordinates)
-            if cv.waitKey(1) == ord('s') or surface_coordinates: # press q to exit  
+            if cv.waitKey(1) == ord('s'): # press q to exit  
+                print("Surface coordinates: ", surface_coordinates)
                 point_coordinates = self.select_area_in_image(img_roi)
-                print(surface_coordinates, point_coordinates)
+                # print("Point Coordinates", point_coordinates)
                 if surface_coordinates:    
                     pc_surface = self.transform_to_surface(surface_coordinates, point_coordinates) # Point Coordinates in surface frame
-                    print("Point Coordinates in surface frame: ", pc_surface)
+                    # print("Point Coordinates in surface frame: ", pc_surface)
                     grid_cell = self.get_block(pc_surface)
                     print("Grid cell: ", grid_cell)
                 else:
@@ -79,13 +89,15 @@ class GirdDetect():
                     print("Bounding box: ", x, y, w, h)
                     
                     cv.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2) # drawing the bounding box for a rectangle (grid)
+                    cv.putText(img_copy, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+                    cv.putText(img_copy, "Area: " + str(int(area)), (x + w + 20, y + 45), cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+
                     return surface_coordinates
                 else:
                     cv.rectangle(img_copy, (x, y), (x+w, y+h), (0, 0, 255), 2) # drawing the bounding box for other shape (solid waste)
                 # Adding text to the image
                 # At this point, this code is by Co-Pilot so I don't even know what this text is showing
-                cv.putText(img_copy, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
-                cv.putText(img_copy, "Area: " + str(int(area)), (x + w + 20, y + 45), cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+                
     
     def get_block(self, pc_surface):
         """Get the coordinates in the form of a 6X6 grid"""
@@ -100,7 +112,7 @@ class GirdDetect():
         
     def transform_to_surface(self, surface_coordinates, point_coordinates):
         # coodinates is in format [x, y, w, h]
-        return [point_coordinates[0]-surface_coordinates[0], point_coordinates[1] - surface_coordinates[1], point_coordinates[2], point_coordinates[3]]
+        return [point_coordinates[0]-surface_coordinates[0], point_coordinates[1] - surface_coordinates[1], surface_coordinates[2], surface_coordinates[3]]
         
     
     def select_area_in_image(self, img):
@@ -115,16 +127,6 @@ class GirdDetect():
         if cv.waitKey(1) == ord('q'): # press q to exit
             return False
                 
-                
-                
-    
- 
-# New idea:
-# Get the bounding box of the surface       
-# Get the coordinates of the origin of the bounding box (left bottom corner) in camera frame
-# Get a transformation matrix from camera frame to the surface frame
-# Apply the transformation matrix to the coordinates of the recyclable garbage
-# Calculate where it lies in the in the grid (might need a pixel to inch conversion?)
 
 
 
